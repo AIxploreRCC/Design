8a7cf7b1dd8ac4317a790a4b225eb1c5bbb86b79
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 09:51:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -11,13 +11,16 @@ logo2_url = "https://raw.githubusercontent.com/AIxploreRCC/Design/main/images.pn
 
 # Titre de l'application avec logos
 st.markdown(f"""
-    <div style="display: flex; flex-direction: column; align-items: center;">
-        <img src="{logo1_url}" alt="Logo 1" style="width: 60px; height: 60px; margin-bottom: 10px;">
-        <img src="{logo2_url}" alt="Logo 2" style="width: 60px; height: 60px; margin-bottom: 10px;">
+    <div style="display: flex; align-items: center; justify-content: center;">
+        <div style="display: flex; align-items: center; margin-right: 20px;">
+            <img src="{logo1_url}" alt="Logo 1" style="width: 100px; height: 100px;">
+            <img src="{logo2_url}" alt="Logo 2" style="width: 60px; height: 60px; margin-left: 10px;">
+        </div>
         <h1 style="margin: 0; text-align: center;">RenalCheck — RCC Clinical Radiomics Algorithm App</h1>
     </div>
     <hr style="border: 1px solid #ccc;">
 """, unsafe_allow_html=True)
+
 
 # Barre de navigation
 menu = ["Home", "About", "Radiomics Score Generator", "Contact"]
