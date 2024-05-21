aed048cb30e4d58ce2935bfa4988ad292c0ab7ff
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 11:51:28 -0500

    Update home.py

--- home.py
+++ home.py
@@ -21,7 +21,8 @@ optimal_threshold = 3.38141178443309
 
 def home():
     # Title of the Streamlit app
-    st.title("Survival Prediction with the Cox Model")
+    st.title("RenalCheck is an advanced AI algorithm designed to predict post-operative oncological outcomes in patients with clear renal cell carcinoma (RCC). This tool is tailored for patients at intermediate or high risk of recurrence, 
+    specifically those meeting the eligibility criteria of the KEYNOTE 564 trial, including stages pT1b and G3-4, pT3/pT4, and N1.")
 
     # Inputs for the model's variables
     hb = st.selectbox("Hemoglobin Level", options=[0, 1])
