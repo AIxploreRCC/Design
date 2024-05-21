3e4afcf10c6e139ffc5c698e249b5dec9dee2f1a
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 16:14:22 -0500

    Update homee.py

--- homee.py
+++ homee.py
@@ -9,14 +9,19 @@ import os
 # Configurer la page pour utiliser toute la largeur
 st.set_page_config(layout="wide")
 
+
 # CSS personnalisé pour le slider
 st.markdown("""
     <style>
-    .stSlider > div:nth-child(2) > div > div > div > div {
+    .stSlider > div[data-baseweb="slider"] > div {
         background: #000080;  /* Bleu navy */
+    }
+    .stSlider > div[data-baseweb="slider"] > div > div {
+        color: #000080;  /* Bleu navy */
     }
     </style>
     """, unsafe_allow_html=True)
+
 
 # Charger le modèle avec mise en cache
 @st.cache(allow_output_mutation=True)
