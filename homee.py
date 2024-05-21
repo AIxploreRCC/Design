ccc981f22561bc2f617e7ca30a26d9b261292469
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 15:09:29 -0500

    Update homee.py

--- homee.py
+++ homee.py
@@ -122,14 +122,11 @@ def homee():
 
                     risk_score = model_cox.predict(input_df)[0]
                    
-
-                    # Centrer la citation entre les deux colonnes
-                    st.markdown("<h3 style='text-align: center;'>The patient is in the {} group.</h3>".format("High risk" if risk_score >= optimal_threshold else "Low risk"), unsafe_allow_html=True)
-
-
+                    # Déterminer le groupe de risque et ajouter l'icône de feu appropriée
                     risk_group = "High risk" if risk_score >= optimal_threshold else "Low risk"
-                    st.subheader(f"The patient is in the {risk_group} group.")
-
-
+                    risk_color = "red" if risk_score >= optimal_threshold else "green"
+                    icon_html = f"<div style='text-align: center;'><span style='color: {risk_color}; font-size: 50px;'>&#x1F6A9;</span></div>"
+                    st.markdown(icon_html, unsafe_allow_html=True)
+                    st.markdown(f"<h3 style='text-align: center;'>The patient is in the {risk_group} group.</h3>", unsafe_allow_html=True)
                 except Exception as e:
                     st.error(f"Prediction failed: {e}")
