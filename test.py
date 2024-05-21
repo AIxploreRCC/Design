98315cde15dd26896bdbf9da1970010a0ef8e372
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 09:56:54 -0500

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ logo2_url = "https://raw.githubusercontent.com/AIxploreRCC/Design/main/images.pn
 st.markdown(f"""
     <div style="display: flex; align-items: center; justify-content: center;">
         <div style="display: flex; align-items: center; margin-right: 20px;">
-            <img src="{logo1_url}" alt="Logo 1" style="width: 100px; height: 100px;">
+            <img src="{logo1_url}" alt="Logo 1" style="width: 150px; height: 100px;">
             <img src="{logo2_url}" alt="Logo 2" style="width: 60px; height: 60px; margin-left: 10px;">
         </div>
         <h1 style="margin: 0; text-align: center;">RenalCheck — RCC Clinical Radiomics Algorithm App</h1>
