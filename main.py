fe4936fb2bf915a39eaa39499429b4d1dcab3fdf
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 14:21:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("styles.css")
+local_css("styles2.css")
 
 # Titre de l'application avec logos
 st.markdown(f"""
