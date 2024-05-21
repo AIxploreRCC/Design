e5caf166d436f1fb04aca45c58c78e54cc1efbbf
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 09:39:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,27 +3,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from lifelines import KaplanMeierFitter
-
-# Fonction factice pour générer un score radiomique
-def generate_radiomics_score(uploaded_file):
-    return np.random.rand()
+from radiomics_score_generator import radiomics_score_generator
 
 # URL des logos hébergés sur GitHub
-logo1_url = "logo 3.png"
-logo2_url = "images.png"
+logo1_url = "https://raw.githubusercontent.com/votre-utilisateur/votre-repo/branche/path-to-logo1/logo1.png"
+logo2_url = "https://raw.githubusercontent.com/votre-utilisateur/votre-repo/branche/path-to-logo2/logo2.png"
 
 # Titre de l'application avec logos
 st.markdown(f"""
     <div style="display: flex; align-items: center; justify-content: center;">
-        <img src="{logo1_url}" alt="Logo1_url" style="width: 60px; height: 60px; margin-right: 20px;">
+        <img src="{logo1_url}" alt="Logo 1" style="width: 60px; height: 60px; margin-right: 20px;">
         <h1 style="margin: 0;">RenalCheck — RCC Clinical Radiomics Algorithm App</h1>
-        <img src="{logo2_url}" alt="Logo2_url" style="width: 60px; height: 60px; margin-left: 20px;">
+        <img src="{logo2_url}" alt="Logo 2" style="width: 60px; height: 60px; margin-left: 20px;">
     </div>
     <hr style="border: 1px solid #ccc;">
 """, unsafe_allow_html=True)
 
 # Barre de navigation
-menu = ["Home", "About", "Upload", "Contact"]
+menu = ["Home", "About", "Radiomics Score Generator", "Contact"]
 choice = st.sidebar.selectbox("Navigation", menu)
 
 if choice == "Home":
@@ -39,7 +36,7 @@ if choice == "Home":
     st.header("Radiomics Score Generator")
     uploaded_file = st.file_uploader("Upload your scan and segmentation", type=["nii", "nii.gz", "dcm"])
     if uploaded_file is not None:
-        radiomics_score = generate_radiomics_score(uploaded_file)
+        radiomics_score = np.random.rand()  # Utilisation de generate_radiomics_score
         st.write(f"Generated Radiomics Score: {radiomics_score:.2f}")
     else:
         radiomics_score = st.slider('Radiomics Score', 0.0, 1.0, 0.5)
@@ -69,11 +66,8 @@ elif choice == "About":
     Adjust the input variables to see how the survival curve changes.
     """)
 
-elif choice == "Upload":
-    st.header("Upload")
-    st.write("""
-    Upload your scan and segmentation to generate a radiomics score.
-    """)
+elif choice == "Radiomics Score Generator":
+    radiomics_score_generator()
 
 elif choice == "Contact":
     st.header("Contact")
