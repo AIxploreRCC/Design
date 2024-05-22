c20caab39f1ea04bd6e7b8d888331b9ed258be74
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 21:18:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from lifelines import KaplanMeierFitter
-import joblib
+from joblib import load
 from homee import homee
 from radiomicss import radiomicss
 
