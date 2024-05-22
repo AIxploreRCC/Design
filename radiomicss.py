6782d9fb2eebc7f813329a35dd3925ac90be1c94
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 21:27:13 -0500

    Update radiomicss.py

--- radiomicss.py
+++ radiomicss.py
@@ -1,79 +1,3 @@
-# Liste des caractéristiques d'intérêt
-features_of_interest = ['original_firstorder_10Percentile',
- 'original_firstorder_Mean',
- 'original_firstorder_Uniformity',
- 'original_glcm_ClusterTendency',
- 'original_glcm_Idm',
- 'original_glcm_Imc2',
- 'original_glcm_JointEnergy',
- 'original_gldm_LargeDependenceEmphasis',
- 'original_gldm_SmallDependenceLowGrayLevelEmphasis',
- 'original_glrlm_HighGrayLevelRunEmphasis',
- 'original_glrlm_LongRunEmphasis',
- 'original_glrlm_LongRunLowGrayLevelEmphasis',
- 'original_glrlm_RunVariance',
- 'original_glrlm_ShortRunEmphasis',
- 'original_glrlm_ShortRunHighGrayLevelEmphasis',
- 'original_glrlm_ShortRunLowGrayLevelEmphasis',
- 'original_glszm_GrayLevelVariance',
- 'original_glszm_HighGrayLevelZoneEmphasis',
- 'original_glszm_LargeAreaLowGrayLevelEmphasis',
- 'original_glszm_SmallAreaHighGrayLevelEmphasis',
- 'original_glszm_ZoneVariance',
- 'wavelet-LLH_firstorder_Entropy',
- 'wavelet-LLH_firstorder_InterquartileRange',
- 'wavelet-LLH_firstorder_Kurtosis',
- 'wavelet-LLH_glcm_Contrast',
- 'wavelet-LLH_glcm_DifferenceVariance',
- 'wavelet-LLH_glcm_Idm',
- 'wavelet-LLH_glcm_Idn',
- 'wavelet-LLH_glcm_Imc1',
- 'wavelet-LLH_gldm_HighGrayLevelEmphasis',
- 'wavelet-LLH_gldm_LargeDependenceEmphasis',
- 'wavelet-LLH_gldm_SmallDependenceHighGrayLevelEmphasis',
- 'wavelet-LLH_glrlm_GrayLevelNonUniformityNormalized',
- 'wavelet-LLH_glrlm_HighGrayLevelRunEmphasis',
- 'wavelet-LLH_glrlm_LongRunLowGrayLevelEmphasis',
- 'wavelet-LLH_glrlm_RunLengthNonUniformity',
- 'wavelet-LLH_glrlm_RunPercentage',
- 'wavelet-LLH_ngtdm_Busyness',
- 'wavelet-LHL_firstorder_RobustMeanAbsoluteDeviation',
- 'wavelet-LHL_glcm_ClusterTendency',
- 'wavelet-LHL_glcm_Correlation',
- 'wavelet-LHL_glcm_DifferenceEntropy',
- 'wavelet-LHL_glcm_Idmn',
- 'wavelet-LHL_glcm_JointEntropy',
- 'wavelet-LHL_glcm_SumAverage',
- 'wavelet-LHL_gldm_DependenceNonUniformityNormalized',
- 'wavelet-LHL_glrlm_LongRunEmphasis',
- 'wavelet-LHL_glszm_SizeZoneNonUniformityNormalized',
- 'wavelet-LHL_ngtdm_Complexity',
- 'wavelet-LHH_firstorder_RootMeanSquared']
-
-
-
-
-columns_to_remove = [
-    'CT Image Path', 'Segmentation File Path', 'diagnostics_Versions_PyRadiomics', 'diagnostics_Versions_Numpy',
-    'diagnostics_Versions_SimpleITK', 'diagnostics_Versions_PyWavelet', 'diagnostics_Versions_Python',
-    'diagnostics_Configuration_Settings', 'diagnostics_Configuration_EnabledImageTypes',
-    'diagnostics_Image-original_Hash', 'diagnostics_Image-original_Dimensionality',
-    'diagnostics_Image-original_Spacing', 'diagnostics_Image-original_Size', 'diagnostics_Image-original_Mean',
-    'diagnostics_Image-original_Minimum', 'diagnostics_Image-original_Maximum', 'diagnostics_Mask-original_Hash',
-    'diagnostics_Mask-original_Spacing', 'diagnostics_Mask-original_Size', 'diagnostics_Mask-original_BoundingBox',
-    'diagnostics_Mask-original_VoxelNum', 'diagnostics_Mask-original_VolumeNum',
-    'diagnostics_Mask-original_CenterOfMassIndex', 'diagnostics_Mask-original_CenterOfMass',
-    'diagnostics_Image-interpolated_Spacing', 'diagnostics_Image-interpolated_Size',
-    'diagnostics_Image-interpolated_Mean', 'diagnostics_Image-interpolated_Minimum',
-    'diagnostics_Image-interpolated_Maximum', 'diagnostics_Mask-interpolated_Spacing',
-    'diagnostics_Mask-interpolated_Size', 'diagnostics_Mask-interpolated_BoundingBox',
-    'diagnostics_Mask-interpolated_VoxelNum', 'diagnostics_Mask-interpolated_VolumeNum',
-    'diagnostics_Mask-interpolated_CenterOfMassIndex', 'diagnostics_Mask-interpolated_CenterOfMass',
-    'diagnostics_Mask-interpolated_Mean', 'diagnostics_Mask-interpolated_Minimum',
-    'diagnostics_Mask-interpolated_Maximum', 'Number of Features'
-]
-
-
 import streamlit as st
 import SimpleITK as sitk
 import tempfile
@@ -87,16 +11,67 @@ from joblib import load
 from sksurv.ensemble import RandomSurvivalForest
 from radiomics import featureextractor
 
+# Liste des caractéristiques d'intérêt
+features_of_interest = [
+    'original_firstorder_10Percentile', 'original_firstorder_Mean', 
+    'original_firstorder_Uniformity', 'original_glcm_ClusterTendency', 
+    'original_glcm_Idm', 'original_glcm_Imc2', 'original_glcm_JointEnergy', 
+    'original_gldm_LargeDependenceEmphasis', 
+    'original_gldm_SmallDependenceLowGrayLevelEmphasis', 
+    'original_glrlm_HighGrayLevelRunEmphasis', 'original_glrlm_LongRunEmphasis', 
+    'original_glrlm_LongRunLowGrayLevelEmphasis', 'original_glrlm_RunVariance', 
+    'original_glrlm_ShortRunEmphasis', 'original_glrlm_ShortRunHighGrayLevelEmphasis', 
+    'original_glrlm_ShortRunLowGrayLevelEmphasis', 'original_glszm_GrayLevelVariance', 
+    'original_glszm_HighGrayLevelZoneEmphasis', 'original_glszm_LargeAreaLowGrayLevelEmphasis', 
+    'original_glszm_SmallAreaHighGrayLevelEmphasis', 'original_glszm_ZoneVariance', 
+    'wavelet-LLH_firstorder_Entropy', 'wavelet-LLH_firstorder_InterquartileRange', 
+    'wavelet-LLH_firstorder_Kurtosis', 'wavelet-LLH_glcm_Contrast', 
+    'wavelet-LLH_glcm_DifferenceVariance', 'wavelet-LLH_glcm_Idm', 
+    'wavelet-LLH_glcm_Idn', 'wavelet-LLH_glcm_Imc1', 'wavelet-LLH_gldm_HighGrayLevelEmphasis', 
+    'wavelet-LLH_gldm_LargeDependenceEmphasis', 'wavelet-LLH_gldm_SmallDependenceHighGrayLevelEmphasis', 
+    'wavelet-LLH_glrlm_GrayLevelNonUniformityNormalized', 'wavelet-LLH_glrlm_HighGrayLevelRunEmphasis', 
+    'wavelet-LLH_glrlm_LongRunLowGrayLevelEmphasis', 'wavelet-LLH_glrlm_RunLengthNonUniformity', 
+    'wavelet-LLH_glrlm_RunPercentage', 'wavelet-LLH_ngtdm_Busyness', 
+    'wavelet-LHL_firstorder_RobustMeanAbsoluteDeviation', 'wavelet-LHL_glcm_ClusterTendency', 
+    'wavelet-LHL_glcm_Correlation', 'wavelet-LHL_glcm_DifferenceEntropy', 
+    'wavelet-LHL_glcm_Idmn', 'wavelet-LHL_glcm_JointEntropy', 'wavelet-LHL_glcm_SumAverage', 
+    'wavelet-LHL_gldm_DependenceNonUniformityNormalized', 'wavelet-LHL_glrlm_LongRunEmphasis', 
+    'wavelet-LHL_glszm_SizeZoneNonUniformityNormalized', 'wavelet-LHL_ngtdm_Complexity', 
+    'wavelet-LHH_firstorder_RootMeanSquared'
+]
+
+columns_to_remove = [
+    'CT Image Path', 'Segmentation File Path', 'diagnostics_Versions_PyRadiomics', 
+    'diagnostics_Versions_Numpy', 'diagnostics_Versions_SimpleITK', 
+    'diagnostics_Versions_PyWavelet', 'diagnostics_Versions_Python', 
+    'diagnostics_Configuration_Settings', 'diagnostics_Configuration_EnabledImageTypes', 
+    'diagnostics_Image-original_Hash', 'diagnostics_Image-original_Dimensionality', 
+    'diagnostics_Image-original_Spacing', 'diagnostics_Image-original_Size', 
+    'diagnostics_Image-original_Mean', 'diagnostics_Image-original_Minimum', 
+    'diagnostics_Image-original_Maximum', 'diagnostics_Mask-original_Hash', 
+    'diagnostics_Mask-original_Spacing', 'diagnostics_Mask-original_Size', 
+    'diagnostics_Mask-original_BoundingBox', 'diagnostics_Mask-original_VoxelNum', 
+    'diagnostics_Mask-original_VolumeNum', 'diagnostics_Mask-original_CenterOfMassIndex', 
+    'diagnostics_Mask-original_CenterOfMass', 'diagnostics_Image-interpolated_Spacing', 
+    'diagnostics_Image-interpolated_Size', 'diagnostics_Image-interpolated_Mean', 
+    'diagnostics_Image-interpolated_Minimum', 'diagnostics_Image-interpolated_Maximum', 
+    'diagnostics_Mask-interpolated_Spacing', 'diagnostics_Mask-interpolated_Size', 
+    'diagnostics_Mask-interpolated_BoundingBox', 'diagnostics_Mask-interpolated_VoxelNum', 
+    'diagnostics_Mask-interpolated_VolumeNum', 'diagnostics_Mask-interpolated_CenterOfMassIndex', 
+    'diagnostics_Mask-interpolated_CenterOfMass', 'diagnostics_Mask-interpolated_Mean', 
+    'diagnostics_Mask-interpolated_Minimum', 'diagnostics_Mask-interpolated_Maximum', 
+    'Number of Features'
+]
 
 def load_model():
     try:
-        return load('random_survival_forest_model.joblib')
+        return load('/mnt/data/random_survival_forest_model.joblib')
     except Exception as e:
         st.error(f"Failed to load the model: {str(e)}")
         raise
 
 rsf_model = load_model()
-scaler = load('scaler.joblib')
+scaler = load('/mnt/data/scaler.joblib')
 
 def setup_extractor():
     extractor = featureextractor.RadiomicsFeatureExtractor()
@@ -136,19 +111,19 @@ if uploaded_ct and uploaded_seg:
     display_images(ct_image, seg_image, slice_number)
 
 if st.button('Start Feature Extraction'):
-        progress_bar = st.progress(0)
-        extractor = setup_extractor()
-        feature_extraction_result = extractor.execute(ct_image, seg_image)
-        features_df = pd.DataFrame([feature_extraction_result])
-        
-        progress_bar.progress(50)
-        
-        # Further processing
-        features_df.drop(columns=columns_to_remove, inplace=True)
-        selected_features_df = features_df[features_of_interest]
-        
-        progress_bar.progress(100)
-        st.write("Selected Features:", st.dataframe(selected_features_df))
+    progress_bar = st.progress(0)
+    extractor = setup_extractor()
+    feature_extraction_result = extractor.execute(ct_image, seg_image)
+    features_df = pd.DataFrame([feature_extraction_result])
+
+    progress_bar.progress(50)
+
+    # Further processing
+    features_df.drop(columns=columns_to_remove, inplace=True)
+    selected_features_df = features_df[features_of_interest]
+
+    progress_bar.progress(100)
+    st.write("Selected Features:", st.dataframe(selected_features_df))
 
 if 'selected_features_df' in st.session_state and st.button('Calculate RAD-Score for Uploaded Patient'):
     time_points = np.linspace(0, 60, 61)
