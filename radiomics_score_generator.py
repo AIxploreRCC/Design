f8a90b62645f5aa040efcf5607ff9bc8eeeaca55
AI_xplore <98645710+AIxploreRCC@users.noreply.github.com>
2024-05-21 21:04:39 -0500

    Update radiomics_score_generator.py

--- radiomics_score_generator.py
+++ radiomics_score_generator.py
@@ -1,38 +1,57 @@
-import streamlit as st
-import SimpleITK as sitk
-import tempfile
-import os
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-from scipy.integrate import simps
-from sklearn.preprocessing import MinMaxScaler
-from joblib import load
-from sksurv.ensemble import RandomSurvivalForest
-from radiomics import featureextractor
-
 # Liste des caractéristiques d'intérêt
-features_of_interest = [
-    'original_firstorder_10Percentile', 'original_firstorder_Mean', 'original_firstorder_Uniformity',
-    'original_glcm_ClusterTendency', 'original_glcm_Idm', 'original_glcm_Imc2', 'original_glcm_JointEnergy',
-    'original_gldm_LargeDependenceEmphasis', 'original_gldm_SmallDependenceLowGrayLevelEmphasis',
-    'original_glrlm_HighGrayLevelRunEmphasis', 'original_glrlm_LongRunEmphasis',
-    'original_glrlm_LongRunLowGrayLevelEmphasis', 'original_glrlm_RunVariance', 'original_glrlm_ShortRunEmphasis',
-    'original_glrlm_ShortRunHighGrayLevelEmphasis', 'original_glrlm_ShortRunLowGrayLevelEmphasis',
-    'original_glszm_GrayLevelVariance', 'original_glszm_HighGrayLevelZoneEmphasis',
-    'original_glszm_LargeAreaLowGrayLevelEmphasis', 'original_glszm_SmallAreaHighGrayLevelEmphasis',
-    'original_glszm_ZoneVariance', 'wavelet-LLH_firstorder_Entropy', 'wavelet-LLH_firstorder_InterquartileRange',
-    'wavelet-LLH_firstorder_Kurtosis', 'wavelet-LLH_glcm_Contrast', 'wavelet-LLH_glcm_DifferenceVariance',
-    'wavelet-LLH_glcm_Idm', 'wavelet-LLH_glcm_Idn', 'wavelet-LLH_glcm_Imc1', 'wavelet-LLH_gldm_HighGrayLevelEmphasis',
-    'wavelet-LLH_gldm_LargeDependenceEmphasis', 'wavelet-LLH_gldm_SmallDependenceHighGrayLevelEmphasis',
-    'wavelet-LLH_glrlm_GrayLevelNonUniformityNormalized', 'wavelet-LLH_glrlm_HighGrayLevelRunEmphasis',
-    'wavelet-LLH_glrlm_LongRunLowGrayLevelEmphasis', 'wavelet-LLH_glrlm_RunLengthNonUniformity',
-    'wavelet-LLH_glrlm_RunPercentage', 'wavelet-LLH_ngtdm_Busyness', 'wavelet-LHL_firstorder_RobustMeanAbsoluteDeviation',
-    'wavelet-LHL_glcm_ClusterTendency', 'wavelet-LHL_glcm_Correlation', 'wavelet-LHL_glcm_DifferenceEntropy',
-    'wavelet-LHL_glcm_Idmn', 'wavelet-LHL_glcm_JointEntropy', 'wavelet-LHL_glcm_SumAverage',
-    'wavelet-LHL_gldm_DependenceNonUniformityNormalized', 'wavelet-LHL_glrlm_LongRunEmphasis',
-    'wavelet-LHL_glszm_SizeZoneNonUniformityNormalized', 'wavelet-LHL_ngtdm_Complexity', 'wavelet-LHH_firstorder_RootMeanSquared'
-]
+features_of_interest = ['original_firstorder_10Percentile',
+ 'original_firstorder_Mean',
+ 'original_firstorder_Uniformity',
+ 'original_glcm_ClusterTendency',
+ 'original_glcm_Idm',
+ 'original_glcm_Imc2',
+ 'original_glcm_JointEnergy',
+ 'original_gldm_LargeDependenceEmphasis',
+ 'original_gldm_SmallDependenceLowGrayLevelEmphasis',
+ 'original_glrlm_HighGrayLevelRunEmphasis',
+ 'original_glrlm_LongRunEmphasis',
+ 'original_glrlm_LongRunLowGrayLevelEmphasis',
+ 'original_glrlm_RunVariance',
+ 'original_glrlm_ShortRunEmphasis',
+ 'original_glrlm_ShortRunHighGrayLevelEmphasis',
+ 'original_glrlm_ShortRunLowGrayLevelEmphasis',
+ 'original_glszm_GrayLevelVariance',
+ 'original_glszm_HighGrayLevelZoneEmphasis',
+ 'original_glszm_LargeAreaLowGrayLevelEmphasis',
+ 'original_glszm_SmallAreaHighGrayLevelEmphasis',
+ 'original_glszm_ZoneVariance',
+ 'wavelet-LLH_firstorder_Entropy',
+ 'wavelet-LLH_firstorder_InterquartileRange',
+ 'wavelet-LLH_firstorder_Kurtosis',
+ 'wavelet-LLH_glcm_Contrast',
+ 'wavelet-LLH_glcm_DifferenceVariance',
+ 'wavelet-LLH_glcm_Idm',
+ 'wavelet-LLH_glcm_Idn',
+ 'wavelet-LLH_glcm_Imc1',
+ 'wavelet-LLH_gldm_HighGrayLevelEmphasis',
+ 'wavelet-LLH_gldm_LargeDependenceEmphasis',
+ 'wavelet-LLH_gldm_SmallDependenceHighGrayLevelEmphasis',
+ 'wavelet-LLH_glrlm_GrayLevelNonUniformityNormalized',
+ 'wavelet-LLH_glrlm_HighGrayLevelRunEmphasis',
+ 'wavelet-LLH_glrlm_LongRunLowGrayLevelEmphasis',
+ 'wavelet-LLH_glrlm_RunLengthNonUniformity',
+ 'wavelet-LLH_glrlm_RunPercentage',
+ 'wavelet-LLH_ngtdm_Busyness',
+ 'wavelet-LHL_firstorder_RobustMeanAbsoluteDeviation',
+ 'wavelet-LHL_glcm_ClusterTendency',
+ 'wavelet-LHL_glcm_Correlation',
+ 'wavelet-LHL_glcm_DifferenceEntropy',
+ 'wavelet-LHL_glcm_Idmn',
+ 'wavelet-LHL_glcm_JointEntropy',
+ 'wavelet-LHL_glcm_SumAverage',
+ 'wavelet-LHL_gldm_DependenceNonUniformityNormalized',
+ 'wavelet-LHL_glrlm_LongRunEmphasis',
+ 'wavelet-LHL_glszm_SizeZoneNonUniformityNormalized',
+ 'wavelet-LHL_ngtdm_Complexity',
+ 'wavelet-LHH_firstorder_RootMeanSquared']
+
+
+
 
 columns_to_remove = [
     'CT Image Path', 'Segmentation File Path', 'diagnostics_Versions_PyRadiomics', 'diagnostics_Versions_Numpy',
@@ -53,6 +72,21 @@ columns_to_remove = [
     'diagnostics_Mask-interpolated_Mean', 'diagnostics_Mask-interpolated_Minimum',
     'diagnostics_Mask-interpolated_Maximum', 'Number of Features'
 ]
+
+
+import streamlit as st
+import SimpleITK as sitk
+import tempfile
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from scipy.integrate import simps
+from sklearn.preprocessing import MinMaxScaler
+from joblib import load
+from sksurv.ensemble import RandomSurvivalForest
+from radiomics import featureextractor
+
 
 def load_model():
     try:
@@ -84,42 +118,41 @@ def display_images(ct_image, seg_image, slice_number):
     plt.axis('off')
     st.pyplot(plt)
 
-def radiomics_score_generator():
-    uploaded_ct = st.file_uploader("Upload CT Image", type=["nii", "nii.gz"])
-    uploaded_seg = st.file_uploader("Upload Segmentation Mask", type=["nii", "nii.gz"])
+uploaded_ct = st.file_uploader("Upload CT Image", type=["nii", "nii.gz"])
+uploaded_seg = st.file_uploader("Upload Segmentation Mask", type=["nii", "nii.gz"])
 
-    if uploaded_ct and uploaded_seg:
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.nii.gz') as tmp_ct:
-            tmp_ct.write(uploaded_ct.getvalue())
-            tmp_ct.seek(0)
-            ct_image = sitk.ReadImage(tmp_ct.name)
+if uploaded_ct and uploaded_seg:
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.nii.gz') as tmp_ct:
+        tmp_ct.write(uploaded_ct.getvalue())
+        tmp_ct.seek(0)
+        ct_image = sitk.ReadImage(tmp_ct.name)
 
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.nii.gz') as tmp_seg:
-            tmp_seg.write(uploaded_seg.getvalue())
-            tmp_seg.seek(0)
-            seg_image = sitk.ReadImage(tmp_seg.name)
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.nii.gz') as tmp_seg:
+        tmp_seg.write(uploaded_seg.getvalue())
+        tmp_seg.seek(0)
+        seg_image = sitk.ReadImage(tmp_seg.name)
 
-        slice_number = st.slider('Select Slice', 0, ct_image.GetSize()[2] - 1, ct_image.GetSize()[2] // 2)
-        display_images(ct_image, seg_image, slice_number)
+    slice_number = st.slider('Select Slice', 0, ct_image.GetSize()[2] - 1, ct_image.GetSize()[2] // 2)
+    display_images(ct_image, seg_image, slice_number)
 
-        if st.button('Start Feature Extraction'):
-            progress_bar = st.progress(0)
-            extractor = setup_extractor()
-            feature_extraction_result = extractor.execute(ct_image, seg_image)
-            features_df = pd.DataFrame([feature_extraction_result])
-            
-            progress_bar.progress(50)
-            
-            # Further processing
-            features_df.drop(columns=columns_to_remove, inplace=True)
-            selected_features_df = features_df[features_of_interest]
-            
-            progress_bar.progress(100)
-            st.write("Selected Features:", st.dataframe(selected_features_df))
+if st.button('Start Feature Extraction'):
+        progress_bar = st.progress(0)
+        extractor = setup_extractor()
+        feature_extraction_result = extractor.execute(ct_image, seg_image)
+        features_df = pd.DataFrame([feature_extraction_result])
+        
+        progress_bar.progress(50)
+        
+        # Further processing
+        features_df.drop(columns=columns_to_remove, inplace=True)
+        selected_features_df = features_df[features_of_interest]
+        
+        progress_bar.progress(100)
+        st.write("Selected Features:", st.dataframe(selected_features_df))
 
-        if 'selected_features_df' in st.session_state and st.button('Calculate RAD-Score for Uploaded Patient'):
-            time_points = np.linspace(0, 60, 61)
-            cumulative_hazards = rsf_model.predict_cumulative_hazard_function(st.session_state['selected_features_df'])
-            rad_scores = np.array([simps([ch(tp) for tp in time_points], time_points) for ch in cumulative_hazards])
-            normalized_rad_scores = scaler.transform(rad_scores.reshape(-1, 1)).flatten()
-            st.write(f"Normalized RAD-Score for the uploaded patient: {normalized_rad_scores[0]:.5f}")
+if 'selected_features_df' in st.session_state and st.button('Calculate RAD-Score for Uploaded Patient'):
+    time_points = np.linspace(0, 60, 61)
+    cumulative_hazards = rsf_model.predict_cumulative_hazard_function(st.session_state['selected_features_df'])
+    rad_scores = np.array([simps([ch(tp) for tp in time_points], time_points) for ch in cumulative_hazards])
+    normalized_rad_scores = scaler.transform(rad_scores.reshape(-1, 1)).flatten()
+    st.write(f"Normalized RAD-Score for the uploaded patient: {normalized_rad_scores[0]:.5f}")
